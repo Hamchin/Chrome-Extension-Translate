7afd5dfb1f6a07f38bf460faf8afbfec05339cab
KeyTey <k.terada.1211@gmail.com>
2020-11-06 00:02:00 +0900

    Update

--- src/google/content.js
+++ src/google/content.js
@@ -1,16 +1,4 @@
-// キーイベント: 言語選択
-$(document).on('keydown', '.tl-wrap', (e) => {
-    // 左キー -> フォーカスの逆移動
-    if (e.keyCode === 37) {
-        $(':focus').prev().focus();
-    }
-    // 右キー -> フォーカスの移動
-    if (e.keyCode === 39) {
-        $(':focus').next().focus();
-    }
-});
-
-// キーイベント: 全体
+// キーイベント
 $(document).on('keydown', (e) => {
     // Escキー -> フォーカス解除
     if (e.keyCode === 27) {
@@ -21,20 +9,9 @@ $(document).on('keydown', (e) => {
     if ($(':focus').length > 0) {
         return true;
     }
-    const btnList = $('.tl-wrap').find('.jfk-button');
-    // 左キー -> 言語選択(左)へフォーカス
-    if (e.keyCode === 37) {
-        $(btnList)[0].focus();
-        return true;
-    }
-    // 右キー -> 言語選択(右)へフォーカス
-    if (e.keyCode === 39) {
-        $(btnList)[1].focus();
-        return true;
-    }
     // Enterキー -> テキストエリアへフォーカス
     if (e.keyCode === 13) {
-        const source = $('#source');
+        const source = $('textarea');
         // Shift + Enter -> テキスト整形
         if (e.shiftKey) {
             const text = $(source).val();
