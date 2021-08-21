60cd99ea72f5120040f8fb2d3b07054c09d4a9a9
Hamchin <k.terada.1211@gmail.com>
2021-08-21 23:22:50 +0900

    Update

--- background/script.js
+++ background/script.js
@@ -11,12 +11,26 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     }
     // テキストをDeepL翻訳する
     if (message.type === 'DEEPL_TRANSLATE') {
-        const url = new URL(DEEPL_TRANSLATE_API_URL + '/translate');
-        url.search = new URLSearchParams(message.data);
+        const url = new URL('https://api-free.deepl.com/v2/translate');
+        url.search = new URLSearchParams({
+            auth_key: DEEPL_AUTH_KEY,
+            text: message.data.text,
+            target_lang: 'JA'
+        });
         fetch(url.toString())
             .then(response => response.ok ? response.json() : null)
-            .then(data => sendResponse(data));
+            .then(data => {
+                const source = message.data.text;
+                const target = data?.translations?.[0]?.text;
+                sendResponse(target ? { source, target } : null);
+            });
         return true;
+        // const url = new URL(DEEPL_TRANSLATE_API_URL);
+        // url.search = new URLSearchParams(message.data);
+        // fetch(url.toString())
+        //     .then(response => response.ok ? response.json() : null)
+        //     .then(data => sendResponse(data));
+        // return true;
     }
 });
 
--- config/config.example.js
+++ config/config.example.js
@@ -1,2 +1,3 @@
-const DEEPL_TRANSLATE_API_URL = 'https://XXXXXXXXXX.execute-api.ap-northeast-1.amazonaws.com/v1';
+const DEEPL_AUTH_KEY = 'XXXXXXXXXX';
+const DEEPL_TRANSLATE_API_URL = 'https://XXXXXXXXXX.execute-api.ap-northeast-1.amazonaws.com/v1/translate';
 const GOOGLE_TRANSLATE_API_URL = 'https://script.google.com/macros/s/XXXXXXXXXX/exec';
